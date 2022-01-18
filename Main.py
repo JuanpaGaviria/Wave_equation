ec4b3c6c00242169dc14e107787811207d6c3365
JPGaviriaC <juanpablogaviria123@gmail.com>
2022-01-18 10:02:29 -0500

    Database (csv) creation

--- Main.py
+++ Main.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 import winsound
 from src import Bigbang
 
-test = Bigbang.big_bang(2,2,3,4,5)
-
-
+"""material selection
+0, 2: anode charged, anode discharged 
+1, 3: cathode charged, cathode discharged
+6: separator
+4,5,7,8,9: others (check csv)
+10, 11: benzene electrolyte, carbon tetrachloride electrolyte 
+"""
+indexes = [0, 1]  # materials definition
+layer_number = 10  # Layers conformed by the materials defined
+url = './src/database/materials_properties.csv'
+df = pd.read_csv(url)
 
