ddd6d62faa8d7055357c272be6e69690456bf78b
JPGaviriaC <juanpablogaviria123@gmail.com>
2022-01-18 10:04:10 -0500

    creation of battery_construction

--- src/boundary/material.py
+++ src/boundary/material.py
@@ -1,14 +1,15 @@
 
-class Material():
-    #constructor
+class Material:
+    # Constructor
     def __init__(self, density, e_modulus, state, bulk_modulus, thickness):
-        self.density=density
-        self.e_modulus=e_modulus
-        self.state=state
-        self.bulk_modulus=bulk_modulus
-        self.thickness=thickness
+        self.density = density
+        self.e_modulus = e_modulus
+        self.state = state
+        self.bulk_modulus = bulk_modulus
+        self.thickness = thickness
         if self.state == 'liquid':
-            self.square_velocity=self.bulk_modulus/self.density
+            self.square_velocity = self.bulk_modulus/self.density
         else:
-            self.square_velocity=self.e_modulus/self.density
+            self.square_velocity = self.e_modulus/self.density
+
 
