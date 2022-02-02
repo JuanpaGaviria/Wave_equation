48acbf1ee7a467a8a97d1fde00eaafd00b33c61f
nico760 <84546260+nico760@users.noreply.github.com>
2022-02-02 09:57:07 -0500

    Update image_generator.py

--- solution_comparison/image_generator.py
+++ solution_comparison/image_generator.py
@@ -1,19 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-dpi = 100
-fz = (5, 5)
+#dpi = 100
+#fz = (5, 5)
 _He = np.genfromtxt('explicit_solution.txt', delimiter=' ')
 _Hi = np.genfromtxt('implicit_solution.txt', delimiter=' ')
 _Ha = np.genfromtxt('analytical_solution.txt', delimiter=' ')
+_H = np.genfromtxt('implicit_solution_fivepoints.txt', delimiter=' ')
 x = np.linspace(0, 1, _He.shape[0])
-fig = plt.figure(dpi=dpi, figsize=fz)
-plt.plot(x, _He[:, 0], lw=1.5, color='k', label= 'Iteration 0 explicit')
-plt.plot(x, _Hi[:, 0], lw=1.5, color='r', marker='o', label= 'Iteration 0 implicit')
-plt.plot(x, _Ha[:, 0], lw=1.5, color='b', marker = '*',label='Iteration 0 analytical')
-plt.plot(x, _He[:, 500], lw=1.5, color='k', label= 'Iteration 500 explicit')
-plt.plot(x, _Hi[:, 500], lw=1.5, color='r',marker= 'o', label= 'Iteration 500 implicit')
-plt.plot(x, _Ha[:, 500], lw=1.5, color='b',marker= '*', label= 'Iteration 500 analytical')
-plt.grid()
-plt.legend()
+fig, axs = plt.subplots(2)
+fig.suptitle('Wave equation solution')
+axs[0].set_title('Solution at initial time')
+axs[0].plot(x, _He[:, 0], lw=1.5, color='k', label= 'explicit')
+#axs[0].plot(x, _Hi[:, 0], lw=1.5, color='r', label= 'implicit')
+axs[0].plot(x, _H[:, 0], lw=1.5, color='r', label= 'implicit_5p')
+axs[0].plot(x, _Ha[:, 0], lw=1.5, color='b', marker = '*',label='analytical')
+
+axs[1].set_title('Solution at 5 s')
+axs[1].plot(x, _He[:, 500], lw=1.5, color='k', label= 'explicit')
+#axs[1].plot(x, _Hi[:, 500], lw=1.5, color='r', label= 'implicit')
+axs[1].plot(x, _H[:, 500], lw=1.5, color='r', label= 'implicit_5p')
+axs[1].plot(x, _Ha[:, 500], lw=1.5, color='b',marker= '*', label= 'analytical')
+
+
+axs[1].set_xlabel('x')
+axs[0].set_ylabel('u')
+axs[1].set_ylabel('u')
+axs[0].grid()
+axs[1].grid()
+axs[0].legend()
+axs[1].legend()
+
+# set the spacing between subplots
+fig.tight_layout()
+
 plt.show()
